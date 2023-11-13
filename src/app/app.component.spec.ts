33765370633e5c5d40f7decb01791e527ecc0de0
giulia-lir <giulia.libe3@gmail.com>
2023-11-13 10:38:29 +0200

    updated test cases in main app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Nordea_Angular_Task' title`, () => {
+  it(`should have the 'Angular_Task' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Nordea_Angular_Task');
+    expect(app.title).toEqual('Angular_Task');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Nordea_Angular_Task');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular_Task');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Nordea_Angular_Task';
+  title = 'Angular_Task';
 }
